8bc729bdd008179dd5b75b399925b9efd4ea0977
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-09-14 13:15:00 +0200

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -1,50 +1,75 @@
-# HelloID-Task-SA-Target-ExchangeOnline-DistributionGroupCreate
-###############################################################
+# HelloID-Task-SA-Target-ExchangeOnline-DistributionGroupGrantMembership
+########################################################################
 # Form mapping
 $formObject = @{
-    Name               = $form.Name
-    DisplayName        = $form.DisplayName
-    PrimarySmtpAddress = $form.PrimarySmtpAddress
-    Alias              = $form.Alias
+    Identity   = $form.Name
+    UsersToAdd = $form.UsersToAdd.Name
 }
+
 [bool]$IsConnected = $false
 try {
-    Write-Information "Executing ExchangeOnline action: [DistributionGroupCreate] for: [$($formObject.DisplayName)]"
+    Write-Information "Executing ExchangeOnline action: [DistributionGroupGrantMembership] for: [$($formObject.Identity)]"
 
     $null = Import-Module ExchangeOnlineManagement
 
     $securePassword = ConvertTo-SecureString $ExchangeOnlineAdminPassword -AsPlainText -Force
     $credential = [System.Management.Automation.PSCredential]::new($ExchangeOnlineAdminUsername, $securePassword)
-    $null = Connect-ExchangeOnline -Credential $credential -ShowBanner:$false -ShowProgress:$false -ErrorAction Stop -Verbose:$false -CommandName 'New-DistributionGroup', 'Disconnect-ExchangeOnline'
+    $null = Connect-ExchangeOnline -Credential $credential -ShowBanner:$false -ShowProgress:$false -ErrorAction Stop -Verbose:$false -CommandName 'Add-DistributionGroupMember', 'Disconnect-ExchangeOnline'
     $IsConnected = $true
 
-    $createdDistributionGroup = New-DistributionGroup @formObject -ErrorAction Stop
+    foreach ($user in $formObject.usersToAdd) {
+        $AddDistributionGroupMember = @{
+            Identity                        = $formObject.Identity
+            Member                          = $user
+            BypassSecurityGroupManagerCheck = $true
+        }
+        try {
+            $null = Add-DistributionGroupMember @AddDistributionGroupMember -Confirm:$false -ErrorAction Stop
 
-    $auditLog = @{
-        Action            = 'CreateResource'
-        System            = 'ExchangeOnline'
-        TargetIdentifier  = $createdDistributionGroup.ExchangeObjectId
-        TargetDisplayName = $createdDistributionGroup.Name
-        Message           = "ExchangeOnline action: [DistributionGroupCreate] for: [$($formObject.DisplayName)] executed successfully"
-        IsError           = $false
+            $auditLog = @{
+                Action            = 'GrantMembership'
+                System            = 'ExchangeOnline'
+                TargetIdentifier  = $formObject.Identity
+                TargetDisplayName = $formObject.Identity
+                Message           = "ExchangeOnline action: [DistributionGroupGrantMembership] [$($user)] to group [$($formObject.Identity)] executed successfully"
+                IsError           = $false
+            }
+            Write-Information -Tags 'Audit' -MessageData $auditLog
+            Write-Information "ExchangeOnline action: [DistributionGroupGrantMembership] [$($user)] to group [$($formObject.Identity)] executed successfully"
+
+        } catch {
+            if ( $_.Exception.ErrorRecord.CategoryInfo.Reason -eq 'MemberAlreadyExistsException') {
+                $auditLog = @{
+                    Action            = 'GrantMembership'
+                    System            = 'ExchangeOnline'
+                    TargetIdentifier  = $formObject.Identity
+                    TargetDisplayName = $formObject.Identity
+                    Message           = "ExchangeOnline action: [DistributionGroupGrantMembership][$($user)] to group [$($formObject.Identity)] Already Exists"
+                    IsError           = $false
+                }
+                Write-Information -Tags 'Audit' -MessageData $auditLog
+                Write-Information "ExchangeOnline action: [DistributionGroupGrantMembership][$($user)] to group [$($formObject.Identity)] Already Exists"
+                Write-Information "Warning message: $($_.Exception.Message)"
+                continue
+            }
+            throw $_
+        }
     }
-    Write-Information -Tags 'Audit' -MessageData $auditLog
-    Write-Information "ExchangeOnline action: [DistributionGroupCreate] for: [$($formObject.DisplayName)] executed successfully"
 } catch {
     $ex = $_
     $auditLog = @{
-        Action            = 'CreateResource'
+        Action            = 'GrantMembership'
         System            = 'ExchangeOnline'
-        TargetIdentifier  = $formObject.DisplayName
-        TargetDisplayName = $formObject.DisplayName
-        Message           = "Could not execute ExchangeOnline action: [DistributionGroupCreate] for: [$($formObject.DisplayName)], error: $($ex.Exception.Message)"
+        TargetIdentifier  = $formObject.Identity
+        TargetDisplayName = $formObject.Identity
+        Message           = "Could not execute ExchangeOnline action: [DistributionGroupGrantMembership] for: [$($formObject.Identity)], error: $($ex.Exception.Message)"
         IsError           = $true
     }
     Write-Information -Tags 'Audit' -MessageData $auditLog
-    Write-Error "Could not execute ExchangeOnline action: [DistributionGroupCreate] for: [$($formObject.DisplayName)], error: $($ex.Exception.Message)"
+    Write-Error "Could not execute ExchangeOnline action: [DistributionGroupGrantMembership] for: [$($formObject.Identity)], error: $($ex.Exception.Message)"
 } finally {
     if ($IsConnected) {
         $null = Disconnect-ExchangeOnline -Confirm:$false -Verbose:$false
     }
 }
-###############################################################
+########################################################################
